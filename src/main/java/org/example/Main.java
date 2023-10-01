c8ac204296ea961e701888324fe8ca2e700551c8
Anastacio Aguilar <tacho.ags@gmail.com>
2023-09-30 19:11:25 -0600

    Collections

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Anastacio");
+        list.add("Juan");
+        list.add("Maria");
+/*
+        for (String lista:list){
+            System.out.println(lista);
+        }*/
+        list.stream().filter(x -> x.startsWith("M")).forEach(System.out::println);
+
+    }
+}
