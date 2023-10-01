e55ea68e193ead447722a1e101027eed9adcd000
Anastacio Aguilar <tacho.ags@gmail.com>
2023-09-30 19:45:22 -0600

    correction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,10 +9,10 @@ public class Main {
         list.add("Anastacio");
         list.add("Juan");
         list.add("Maria");
-/*
+
         for (String lista:list){
             System.out.println(lista);
-        }*/
+        }
         list.stream().filter(x -> x.startsWith("M")).forEach(System.out::println);
 
     }
